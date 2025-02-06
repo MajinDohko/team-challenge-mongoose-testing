0c37467727f4ba8e12b789b1bebc4acf1a563551
MajinDohko <agarridoluengo@gmail.com>
2025-02-06 18:54:08 +0100

    añadidio el primer test

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const PORT = 3000;
 const dbConnection = require('./config/config');
 const router = require('./routes/posts');
 
-app.use('/', router)
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/', router)
 
 dbConnection();
 
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
+
+module.exports = app;
 
 /* 
 const express = require('express');
---
+++ tests/posts.test.js
@@ -0,0 +1,28 @@
+const request = require('supertest');
+const mongoose = require('mongoose');
+const app = require('../index');
+const Post = require('../models/Post');
+
+describe('POST /create', () => {
+    const post = {
+        title: 'título de prueba',
+        body: 'contenido de prueba'
+    };
+
+    beforeEach(async ()=>{
+        await Post.deleteMany();
+    })
+
+    test('Create a Post', async () => {
+        let postsCount = await Post.countDocuments({});
+        expect(postsCount).toBe(0);
+
+        resPost = await request(app).post('/create').send(post).expect(201);
+
+        postsCount = await Post.countDocuments({});
+        expect(postsCount).toBe(1);
+    });
+});
+
+
+
